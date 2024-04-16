ec9f3739ebe6ad01a89e9bac266c69c33bdd8da6
zhu huaxing <zhuhuaxing@thinkerx.com>
2024-04-16 21:06:54 +0800

    fix: 新增taro笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -67,14 +67,18 @@ module.exports = {
       },
       {
         text: "面试题",
-        link: "/test/"
-      }
+        link: "/test/",
+      },
     ],
     sidebar: {
       "/structure/": [
         {
           text: "构建工具",
-          children: ["/structure/README.md"]
+          children: ["/structure/README.md"],
+        },
+        {
+          text: "Taro",
+          children: ["/structure/taro/README.md"],
         },
         {
           text: "webpack",
@@ -108,95 +112,135 @@ module.exports = {
             },
             {
               text: "高级优化",
-              children: ["/structure/webpack/senior/README.md", "/structure/webpack/senior/enhanceExperience.md", "/structure/webpack/senior/liftingSpeed.md", "/structure/webpack/senior/reduceVolume.md", "/structure/webpack/senior/optimizePerformance.md", "/structure/webpack/senior/summary.md"],
+              children: [
+                "/structure/webpack/senior/README.md",
+                "/structure/webpack/senior/enhanceExperience.md",
+                "/structure/webpack/senior/liftingSpeed.md",
+                "/structure/webpack/senior/reduceVolume.md",
+                "/structure/webpack/senior/optimizePerformance.md",
+                "/structure/webpack/senior/summary.md",
+              ],
             },
             {
               text: "项目配置",
-              children: ["/structure/webpack/project/README.md", "/structure/webpack/project/react-cli.md", "/structure/webpack/project/vue-cli.md", "/structure/webpack/project/summary.md"],
+              children: [
+                "/structure/webpack/project/README.md",
+                "/structure/webpack/project/react-cli.md",
+                "/structure/webpack/project/vue-cli.md",
+                "/structure/webpack/project/summary.md",
+              ],
             },
             {
               text: "原理分析",
-              children: ["/structure/webpack/origin/README.md", "/structure/webpack/origin/loader.md", "/structure/webpack/origin/plugin.md", "/structure/webpack/origin/summary.md"],
-            },]
+              children: [
+                "/structure/webpack/origin/README.md",
+                "/structure/webpack/origin/loader.md",
+                "/structure/webpack/origin/plugin.md",
+                "/structure/webpack/origin/summary.md",
+              ],
+            },
+          ],
         },
       ],
       "/react/": [
         {
           // text:'React知识点',
-          children: ['/react/README.md', '/react/UsageTips', '/react/Router', '/react/redux']
-        }
+          children: [
+            "/react/README.md",
+            "/react/UsageTips",
+            "/react/Router",
+            "/react/redux",
+          ],
+        },
       ],
       "/programSkill/": [
         {
           // text:'React知识点',
-          children: ['/programSkill/README.md']
-        }
+          children: ["/programSkill/README.md"],
+        },
       ],
       "/js/": [
         {
           // text:'React知识点',
-          children: ['/js/README.md', '/js/es6', '/js/API', '/js/class', '/js/inherit', '/js/promise','/js/ts']
-        }
+          children: [
+            "/js/README.md",
+            "/js/es6",
+            "/js/API",
+            "/js/class",
+            "/js/inherit",
+            "/js/promise",
+            "/js/ts",
+          ],
+        },
       ],
       "/html/": [
         {
           // text:'React知识点',
-          children: ['/html/README.md']
-        }
+          children: ["/html/README.md"],
+        },
       ],
       "/css/": [
         {
-          text: 'css相关',
-          children: ['/css/README.md', '/css/pretreatment', '/css/layout', '/css/common']
-        }
+          text: "css相关",
+          children: [
+            "/css/README.md",
+            "/css/pretreatment",
+            "/css/layout",
+            "/css/common",
+          ],
+        },
       ],
       "/concept/": [
         {
           // text:'React知识点',
-          children: ['/concept/README.md']
-        }
+          children: ["/concept/README.md"],
+        },
       ],
       "/developIssue/": [
         {
           // text:'React知识点',
-          children: ['/developIssue/README.md']
-        }
+          children: ["/developIssue/README.md"],
+        },
       ],
       "/commonLibrary/": [
         {
           // text:'React知识点',
-          children: ['/commonLibrary/README.md']
-        }
+          children: ["/commonLibrary/README.md"],
+        },
       ],
       "/network/": [
         {
-          text:'网络相关',
-          children: ['/network/README.md','/network/Ajax.md','/network/http.md']
-        }
+          text: "网络相关",
+          children: [
+            "/network/README.md",
+            "/network/Ajax.md",
+            "/network/http.md",
+          ],
+        },
       ],
       "/Vue/": [
         {
-          text: 'Vue相关',
-          children: ['/Vue/README.md', '/Vue/vue3.md']
-        }
+          text: "Vue相关",
+          children: ["/Vue/README.md", "/Vue/vue3.md"],
+        },
       ],
       "/test/": [
         {
           // text: '面试题',
-          children: ['/test/README.md']
-        }
+          children: ["/test/README.md"],
+        },
       ],
       "/WebAPI/": [
         {
-          text: 'WebAPI',
-          children: ['/WebAPI/README.md']
-        }
+          text: "WebAPI",
+          children: ["/WebAPI/README.md"],
+        },
       ],
       "/node/": [
         {
-          text: 'node',
-          children: ['/node/README.md']
-        }
+          text: "node",
+          children: ["/node/README.md"],
+        },
       ],
     },
   },
---
+++ docs/structure/taro/README.md
@@ -0,0 +1,7 @@
+# Taro
+
+一个打包多端小程序的框架
+
+## 项目目录
+
+<img src="../../.vuepress/public/imgs/structure/taro_list.jpg" alt="目录结构">
